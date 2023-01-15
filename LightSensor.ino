3f1687e76881df39856a2570cfcf09dd55c40a27
Chae-Rie <yoospam@protonmail.com>
2023-01-15 21:01:32 +0100

    add: Einfaches Beispiel mit am Code definierten Fragen zu kommender Bearbeitung

---
+++ LightSensor.ino
@@ -0,0 +1,24 @@
+// Vorab definierte Konstanten
+
+const int LIGHT_SENSOR_PIN = A0; 
+const int LED_PIN          = 3;  
+// Können die PIN-Belegungen so bleiben?
+const int ANALOG_THRESHOLD = 500; // Ab welchem Wert soll die LED angehen?
+int analogValue = 0;
+
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT); // set arduino pin to output mode
+}
+
+void loop() {
+  analogValue = analogRead(LIGHT_SENSOR_PIN);
+  
+  if(analogValue < ANALOG_THRESHOLD)
+    digitalWrite(LED_PIN, HIGH);
+  else
+    digitalWrite(LED_PIN, LOW); 
+
+    // Einfachste Möglichkeit die Werte abzufragen und zu vergleichen. Wollen wir ggf.
+    // ein delay nutzen? 
+}
