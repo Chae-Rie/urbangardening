6388c788e9fd63c2f0c2df8aeaeb3d547d7fb8a9
Chae-Rie <yoospam@protonmail.com>
2023-02-04 23:25:59 +0100

    add: Hinzufügen einer ersten GUI out of box
    add: Hinzufügen des MessageCode.h
    -> Erschaffen einer klaren Struktur, wichtig für WM (Windows Messages)

---
+++ UrbanGardening/MessageCodes.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Hier werden alle Messagecodes eingefügt auf die von der jeweiligen Klasse zugegriffen werden muss
+
+// Messagecodes werden Relativ zu WM_APP erstellt
+// z.B.
+// #define CHANGE_ACCESS			(WM_APP + 1)
