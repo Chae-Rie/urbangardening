06f4fbd931e4b01de017527faf700a7be8dbb7ee
Chae-Rie <yoospam@protonmail.com>
2023-03-07 23:35:08 +0100

    add: Hinzufügen des Test-Snippets für den Arduino

---
+++ Snippets/Serial_Snippets/MyAnalogReadSerial.ino
@@ -0,0 +1,29 @@
+/*
+  AnalogReadSerial
+
+  Reads an analog input on pin 0, prints the result to the Serial Monitor.
+  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
+*/
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  char input = Serial.read();
+  if (input == "Test123") {
+    Serial.print("Succeed");
+    
+  };
+  Serial.println(input);
+  delay(1);        // lay in between reads for stability// de
+  
+}
